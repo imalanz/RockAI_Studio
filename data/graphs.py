3350e1869fe9da91d15799b6367050351bd09236
imalanz <arq.imalanz@gmail.com>
2023-02-13 11:34:44 +0100

    mond

--- data/graphs.py
+++ data/graphs.py
@@ -19,12 +19,12 @@ def clean_cities_df (df):
     return x
 
 # get the coordinates for carto maps.
-def coor_points (df):
+def carto_points (df):
     return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["longitude"], df["latitud"]))
 
 # Filter on carto maps for music df.
 def carto_music (df):
-    x = Map(Layer(df, basic_style(size=15, opacity=10, color="#008080"),legends=default_legend('music')))
+    x = Map(Layer(df, basic_style(size=15, opacity=10, color="#008080"),legends=default_legend('music companies')))
     return x
 
 # carto maps for startups.
@@ -32,34 +32,37 @@ def carto_startups (df):
     x = Map(Layer(df, basic_style(size=15, color="#b4c8a8"), legends=default_legend("Startups")))
     return x
 
+# get the coordinates for carto maps in starbucks.
+def starbucks_carto_points (df):
+    return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["longitude"], df["latitude"]))
 # carto maps for starbucks.
-def carto_startups (df):
-    x = Map(Layer(df, basic_style(size=15, color="#70a494"), legends=default_legend("Startups")))
+def carto_starbucks (df):
+    x = Map(Layer(df, basic_style(size=15, color="#70a494"), legends=default_legend("Starbucks")))
     return x
 
 # carto maps for schools.
 def carto_schools (df):
-    x = Map(Layer(df, basic_style(size=15, color="#f6edbd"), legends=default_legend("Startups")))
+    x = Map(Layer(df, basic_style(size=15, color="#f6edbd"), legends=default_legend("Schools")))
     return x
 
 # carto maps for bars.
-def carto_schools (df):
-    x = Map(Layer(df, basic_style(size=15, color="#edbb8a"), legends=default_legend("Startups")))
+def carto_bars (df):
+    x = Map(Layer(df, basic_style(size=15, color="#edbb8a"), legends=default_legend("Bars")))
     return x
 
 # carto maps for concerts.
 def carto_concerts (df):
-    x = Map(Layer(df, basic_style(size=15, color="#de8a5a"), legends=default_legend("Startups")))
+    x = Map(Layer(df, basic_style(size=15, color="#de8a5a"), legends=default_legend("Concerts")))
     return x
 
 # carto maps multiple layers.
 def carto_layers (df1, df2, df3, df4, df5, df6):
     from palettable.cartocolors.diverging import Geyser_7
-    x = Map([Layer(music_points, basic_style(size=15, opacity=10, color="#008080"),legends=default_legend('music')),
-        Layer(starbucks, basic_style(size=15, opacity=10, color="#70a494"), legends=default_legend("starbucks")),
-        Layer(startups, basic_style(size=15, opacity=10, color="#b4c8a8"), legends=default_legend("Startups")),
-        Layer(schools, basic_style(size=15, opacity=10, color="#f6edbd"), legends=default_legend("schools")),
-        Layer(bars, basic_style(size=15, opacity=10, color="#edbb8a"), legends=default_legend("bars")),
-        Layer(concert, basic_style(size=15, opacity=10, color="#de8a5a"), legends=default_legend("concerts"))], layer_selector=True)
+    x = Map([Layer(df1, basic_style(size=15, opacity=10, color="#008080"),legends=default_legend('music')),
+        Layer(df2, basic_style(size=15, opacity=10, color="#70a494"), legends=default_legend("starbucks")),
+        Layer(df3, basic_style(size=15, opacity=10, color="#b4c8a8"), legends=default_legend("Startups")),
+        Layer(df4, basic_style(size=15, opacity=10, color="#f6edbd"), legends=default_legend("schools")),
+        Layer(df5, basic_style(size=15, opacity=10, color="#edbb8a"), legends=default_legend("bars")),
+        Layer(df5, basic_style(size=15, opacity=10, color="#de8a5a"), legends=default_legend("concerts"))], layer_selector=True)
     return x
 
